d330a83ea61a171379ffa8c73a4cf426ce3974a9
urunov <urunovfirdavsjon@gmail.com>
2021-12-14 10:06:45 +0500

    day 2 refactoring

--- codeforces/day2/263A - Красивая матрица/main.py
+++ codeforces/day2/263A - Красивая матрица/main.py
@@ -1,9 +1,9 @@
+lengthMatrix = 5
 matrix = []
-count = 0
 x = 0
 y = 0
 
-for i in range(5):
+for i in range(lengthMatrix):
     matrix.append(list(map(int, input().split())))
     try:
         x = matrix[i].index(1)
@@ -12,18 +12,4 @@ for i in range(5):
     except:
         continue
 
-while x != 2:
-    if x >= 3:
-        x -= 1
-    elif x <= 3:
-        x += 1
-    count += 1
-
-while y != 2:
-    if y >= 3:
-        y -= 1
-    elif y <= 3:
-        y += 1
-    count += 1
-
-print(count)
+print(abs(abs(x - lengthMatrix) - abs(y - lengthMatrix)))
