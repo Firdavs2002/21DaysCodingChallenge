9434e3066ca8dd171f3828d65ccfa090b88e778b
urunov <urunovfirdavsjon@gmail.com>
2021-12-19 23:41:21 +0500

    refactoring for day 7

--- codeforces/day7/510A - Лиса и змейка/main.py
+++ codeforces/day7/510A - Лиса и змейка/main.py
@@ -3,13 +3,12 @@ snake = ''
 count = 0;
 
 for i in range(n):
-    count+=1;
+    count+=1
     for j in range(m):
-        if i % 2 == 0 or ( count % 4 != 0  and j == m-1) or (count % 4 == 0 and j == 0):
+        if i % 2 == 0 or (count % 4 != 0  and j == m-1) or (count % 4 == 0 and j == 0):
             snake += '#'
         else:
             snake += '.'
-        
         
     snake += '\n'
     print(snake, end='')
